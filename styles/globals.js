ef4a6a4824be63356227926d3980373d09684655
sankthomas <thomassankara74@gmail.com>
2023-08-04 13:19:28 +0300

    globals styles

---
+++ styles/globals.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+export const globals = StyleSheet.create({
+  heading: {
+    fontFamily: "productsansbold",
+  },
+  heading2: {
+    fontFamily: "productsansbold",
+  },
+  text: {
+    fontFamily: "productsansregular",
+  },
+  textDark: {
+    fontFamily: "productsansregular",
+  },
+  blue: {
+    fontFamily: "productsansbold",
+  },
+  button: {
+    fontFamily: "productsansregular",
+  },
+});
